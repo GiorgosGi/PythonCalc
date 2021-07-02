5811df906cc222f92e7c619d44ded7a01caaba61
GiorgosGi <73617921+GiorgosGi@users.noreply.github.com>
2021-07-02 22:10:34 +0300

    Add files via upload

---
+++ pythonCalc2.py
@@ -0,0 +1,30 @@
+import re
+
+print('Simple calculator')
+print('Type "end" to exit')
+
+previous = 0
+run = True
+
+def calculate():
+    global run
+    global previous
+    equation = ""
+    if previous ==0:
+        equation = input("Enter calculation: ")
+    else:
+        equation = input(str(previous))
+
+    if equation == "end":
+        print("Goodbye")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+    if previous == 0:
+        previous = eval(equation)
+    else:
+        previous = eval(str(previous) + equation)
+
+while run:
+    calculate()
